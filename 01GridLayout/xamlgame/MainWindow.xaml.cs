2d9825fc87370f56e9611834e0799f7ddf7923cd
Hives Péter <hivesp@gmail.com>
2019-01-27 00:26:44 +0100

    esemény vezérlő hozzáadása

--- 01GridLayout/xamlgame/MainWindow.xaml.cs
+++ 01GridLayout/xamlgame/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,16 @@ namespace xamlgame
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonNo_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("buttonNo");
+        }
+
+        private void ButtonYes_Click(object sender, RoutedEventArgs e)
+        {
+            Debug.WriteLine("buttonYes");
         }
     }
 }
